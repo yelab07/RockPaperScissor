5e80f1edefd3f4e67e78003bf0b863fe20de89d4
yelab07 <79489573+yelab07@users.noreply.github.com>
2021-12-02 15:21:51 -0600

    first commit

---
+++ Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAiSfgiwuXIgrivqgJpXNmjUQhZRFBcPQE",
+  authDomain: "gameapp-a116e.firebaseapp.com",
+  projectId: "gameapp-a116e",
+  storageBucket: "gameapp-a116e.appspot.com",
+  messagingSenderId: "568921495042",
+  appId: "1:568921495042:web:0310d90f6436fc4d87f91b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
